e581cb19c0ad28d39044d526995ee72f6147fa63
onsachin <connect@onsachin.com>
2025-02-10 20:45:09 +0530

    Update Program.cs

--- WebApiOne.Api/Program.cs
+++ WebApiOne.Api/Program.cs
@@ -1,3 +1,5 @@
+using WebApiOne.Api.Operations;
+
 namespace WebApiOne.Api;
 
 public class Program
@@ -9,24 +11,45 @@ public class Program
         // Add services to the container.
 
         builder.Services.AddControllers();
+        builder.Services.AddScoped(typeof(IOperationHandler<>), typeof(OperationHandler<>));
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        //builder.Services.AddOpenApi();
-
         var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        app.MapGet("log/ex1", () =>
         {
+            throw new NotImplementedException();
+        });
+        
+        app.MapGet("log/ex2", () =>
+        {
+            throw new ArgumentNullException();
+        });
+        app.MapGet("log/ex3", () =>
+        {
+            throw new ArgumentOutOfRangeException();
+        });
+        app.MapGet("log/ex4", () =>
+        {
+            throw new UnauthorizedAccessException();
+        });
+        app.MapGet("log/ex5", () =>
+        {
+            throw new DivideByZeroException();
+        });
+        app.MapGet("log/ex6", () =>
+        {
+            throw new DataMisalignedException();
+        });
+        app.MapGet("log/ex7", () =>
+        {
+            throw new NullReferenceException();
             
-        }
-
-        //app.UseHttpsRedirection();
-
+        });
+        
         app.UseAuthorization();
 
 
-        app.MapControllers();
-
+        app.MapControllers(); 
+        app.UseMiddleware<UseExceptionMiddleware>();
         app.Run();
     }
 }
